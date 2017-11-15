f402d5f6da01adcd538d6c4840115ca81f45869c
michaelbrawer <michaelbrawer@me.com>
2017-11-14 18:37:39 -0800

    add method override

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+
 
 var index = require('./routes/index');
 var users = require('./routes/users');
