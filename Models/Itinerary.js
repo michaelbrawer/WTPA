f8c2815041f2fdde929fc731a56e3c702a180b4f
michaelbrawer <michaelbrawer@me.com>
2017-11-14 19:07:54 -0800

    config model exports

--- Models/Itinerary.js
+++ Models/Itinerary.js
@@ -1,3 +1,7 @@
 var mongoose = require('mongoose');
 
 var Schema = mongoose.Schema; 
+
+//schema goes here
+
+module.exports = mongoose.model("Itinerary", itinerarySchema);
--- Models/Stop.js
+++ Models/Stop.js
@@ -2,3 +2,6 @@ var mongoose = require('mongoose');
 
 var Schema = mongoose.Schema; 
 
+//schema goes here
+
+module.exports = mongoose.model("Stop", stopSchema);
