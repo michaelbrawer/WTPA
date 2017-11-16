6072a38ffec4832594b2c8605b166610bfd4cfab
Devin Fox <devinfoxbusiness@gmail.co>
2017-11-15 22:27:37 -0800

    models made

--- Models/Itinerary.js
+++ Models/Itinerary.js
@@ -2,6 +2,11 @@ var mongoose = require('mongoose');
 
 var Schema = mongoose.Schema; 
 
+var mongooseSchema = new Schema({
+    admin: [],
+    date: String,
+    stops: [{type: mongoose.Schema.Types.ObjectId, ref: "Stop"}]
+});
 //schema goes here
 
 module.exports = mongoose.model("Itinerary", itinerarySchema);
