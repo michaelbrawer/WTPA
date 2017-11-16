564e47f59172863c7f357ced955299c36b46d3ad
michaelbrawer <michaelbrawer@me.com>
2017-11-15 19:01:02 -0800

    stubbed up api-itinerary controller

--- controllers/api/api_itinerariesCTRL.js
+++ controllers/api/api_itinerariesCTRL.js
@@ -1,1 +1,17 @@
 var Itinerary = require('../models/itinerary');
+
+var Itinerary = require('../models/Itinerary');
+
+
+function getAllItineraries(req, res){
+  res.render('itineraries.show');
+}
+
+function getOneItinerary(req, res){
+
+}
+
+module.exports = {
+  getAllItineraries,
+  getOneItinerary
+};
