10056952554cea040b03bbba2bf1162cffb55d7e
michaelbrawer <michaelbrawer@me.com>
2017-11-17 11:31:23 -0800

    added auth related modules

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var passport = require('passport');
 var yelpCtrl = require('./../controllers/yelpCtrl.js')
 
 /* GET home page. */
--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var passport = require('passport');
 var usersCtrl = require('../controllers/usersCtrl');
 
 /* GET users listing. */
