6ac65d67204a42869c1de430436310bd61cfb565
Alonso Tacanga <alonso.tacanga@gmail.com>
2017-11-22 01:34:12 -0800

    added comments functions

--- routes/party.js
+++ routes/party.js
@@ -2,16 +2,20 @@ var express = require('express');
 var router = express.Router();
 var passport = require('passport');
 var stopsCtrl = require('../controllers/stopsCtrl');
+var usersCtrl = require('../controllers/usersCtrl');
 
 
 /* GET users listing. */
 router.get('/new', stopsCtrl.new);
 router.get('/move', stopsCtrl.move)
 router.get('/:id/', stopsCtrl.index);
+router.post('/comment', usersCtrl.createComment);
+router.delete('/out', usersCtrl.removeComment);
 router.post('/:id', stopsCtrl.add);
 router.post('/', stopsCtrl.create);
 router.delete('/all', stopsCtrl.deleteAll)
 router.delete('/:id', stopsCtrl.remove);
+
 
 
 module.exports = router;
