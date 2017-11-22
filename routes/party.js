32d36157503a730f412b67d921120c73164ebdca
michaelbrawer <michaelbrawer@me.com>
2017-11-22 11:23:11 -0800

    sc tidy

--- routes/party.js
+++ routes/party.js
@@ -7,13 +7,13 @@ var usersCtrl = require('../controllers/usersCtrl');
 
 /* GET users listing. */
 router.get('/new', stopsCtrl.new);
-router.get('/move', stopsCtrl.move)
+router.get('/move', stopsCtrl.move);
 router.get('/:id/', stopsCtrl.index);
 router.post('/comment', usersCtrl.createComment);
 router.delete('/out', usersCtrl.removeComment);
 router.post('/:id', stopsCtrl.add);
 router.post('/', stopsCtrl.create);
-router.delete('/all', stopsCtrl.deleteAll)
+router.delete('/all', stopsCtrl.deleteAll);
 router.delete('/:id', stopsCtrl.remove);
 
 
