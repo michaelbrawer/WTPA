b47f52249a11dbea167283ac9b03bef1b037eb19
michaelbrawer <michaelbrawer@me.com>
2017-11-23 01:35:27 -0800

    indent cleanup on controllers

--- controllers/api/users.js
+++ controllers/api/users.js
@@ -1,33 +1,34 @@
 var User = require('../../models/User');
 var Stop = require('../../models/Stop');
 
-function getOneUser (req, res) {
-    User.findById(req.params.id, function(err, user) {
-        res.status(200).json(user);
-    })
+function getOneUser(req, res) {
+  User.findById(req.params.id, function(err, user) {
+    res.status(200).json(user);
+  });
 }
 
 function getAllStops(req, res) {
-    Stop.find({}, function(err, stops) {
-        res.status(200).json(stops);
-    });
+  Stop.find({}, function(err, stops) {
+    res.status(200).json(stops);
+  });
 }
 
 function oneStop(req, res) {
-    Stop.findById(req.params.id, function(err, stop) {
-        res.status(200).json(stop)
-    });
+  Stop.findById(req.params.id, function(err, stop) {
+    res.status(200).json(stop);
+  });
 }
 
-function getUserStops (req, res) {
-    User.findById(req.params.id).populate('stops').exec(function(err, stops) {    
-        res.status(200).json(stops)}
-)}
-    
+function getUserStops(req, res) {
+  User.findById(req.params.id).populate('stops').exec(function(err, stops) {
+    res.status(200).json(stops);
+  });
+}
+
 
 module.exports = {
-    getOneUser,
-    getAllStops,
-    oneStop,
-    getUserStops
+  getOneUser,
+  getAllStops,
+  oneStop,
+  getUserStops
 };
--- controllers/stopsCtrl.js
+++ controllers/stopsCtrl.js
@@ -14,22 +14,30 @@ module.exports = {
   // deleteAll
 };
 
-function index(req, res){
+function index(req, res) {
   edit = req.query.edit ? req.query.edit : null;
   user = req.user ? req.user : null;
   User.findById(req.params.id).populate('stops').exec(function(err, userPage) {
-      res.render('party/show', {user: user, userPage, stops: userPage.stops, pageId: req.params.id, goBack: false});
+    res.render('party/show', {
+      user: user,
+      userPage,
+      stops: userPage.stops,
+      pageId: req.params.id,
+      goBack: false
+    });
   });
 }
 
-function newUser(req, res){
-  res.render('party/new', {user: req.user});
+function newUser(req, res) {
+  res.render('party/new', {
+    user: req.user
+  });
 }
 
-function addStop(req, res, itin){
+function addStop(req, res, itin) {
   user = req.user;
   userPage = user;
-  edit = req.query.edit ? req.query.edit : null;  
+  edit = req.query.edit ? req.query.edit : null;
   var newStop = new Stop({
     name: req.body.name,
     location: req.body.location,
@@ -40,7 +48,13 @@ function addStop(req, res, itin){
     req.user.stops.push(newStop);
     req.user.save(function(err) {
       req.user.populate('stops', function(err) {
-        res.render('party/show', {user, userPage, stops: req.user.stops, pageId: req.params.id, goBack: false});
+        res.render('party/show', {
+          user,
+          userPage,
+          stops: req.user.stops,
+          pageId: req.params.id,
+          goBack: false
+        });
       });
     });
   });
@@ -48,7 +62,7 @@ function addStop(req, res, itin){
 
 // remove a single stop
 
-function removeStop(req, res){
+function removeStop(req, res) {
   Stop.findByIdAndRemove(req.body.stop_id, function(err, doc) {
     res.redirect(`/party/${req.user._id}`);
   });
@@ -56,7 +70,7 @@ function removeStop(req, res){
 
 // delete all stops from one user
 
-function removeAllStops(req, res){
+function removeAllStops(req, res) {
   req.user.stops = [];
   req.user.save(function(err) {
     res.redirect(`/party/${req.user._id}`);
@@ -67,10 +81,9 @@ function move(req, res) {
   var idx = req.user.stops.indexOf(req.query.id);
   var temp = req.user.stops.splice(idx, 1)[0];
   if (req.query.dir === 'u') {
-    req.user.stops.splice(idx-1, 0, temp);
-  }
-  else if (req.query.dir === 'd') {
-    req.user.stops.splice(idx+1, 0, temp);
+    req.user.stops.splice(idx - 1, 0, temp);
+  } else if (req.query.dir === 'd') {
+    req.user.stops.splice(idx + 1, 0, temp);
   }
   req.user.save(function(err) {
     res.redirect(`/party/${req.user._id}`);
@@ -78,14 +91,14 @@ function move(req, res) {
 }
 
 
-function create(req, res){
+function create(req, res) {
 
 }
 
-function edit(req, res, next){
+function edit(req, res, next) {
 
 }
 
-function update(req, res, next){
+function update(req, res, next) {
 
 }
