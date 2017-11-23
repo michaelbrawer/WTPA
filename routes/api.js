65e2344429c2b9ed61c4d3d009ff38d295758f13
Kevin de Roulet <kderoulet@yahoo.com>
2017-11-23 09:46:23 -0800

    cleans up controllers, models, and routes

--- routes/api.js
+++ routes/api.js
@@ -8,9 +8,4 @@ router.get('/users/:id', usersCtrl.getOneUser);
 router.get('/stops/:id', usersCtrl.oneStop);
 router.get('/users/:id/stops', usersCtrl.getUserStops)
 
-function isLoggedIn(req, res, next) {
-    if (req.isAuthenticated()) return next();
-    res.redirect('/auth/google');
-} 
-
 module.exports = router;
